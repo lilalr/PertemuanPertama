287faa3ec01939631d580cd074737a3bbac09ba3
lilalr <gurennut@gmail.com>
2024-02-27 18:50:02 +0700

    Add project files.

---
+++ PertemuanPertama/PertemuanPertama.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{//begin
+	float nJejari, nLuas;
+	cout << "Masukan jejari= ";
+	cin >> nJejari;
+	nLuas = 3.14 * nJejari * nJejari;
+	cout << "Luasnya = " << nLuas << endl;
+
+	//numeric nJejari, nLuas
+	//display 'Masukan jejari= '
+	//compute nLuas = 3.14 * r * r
+
+}
