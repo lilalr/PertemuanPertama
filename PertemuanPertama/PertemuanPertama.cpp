3274f957f020e43fda2324ee6a6ebcb4067ee529
lilalr <gurennut@gmail.com>
2024-02-27 19:04:07 +0700

    menambah jejari

--- PertemuanPertama/PertemuanPertama.cpp
+++ PertemuanPertama/PertemuanPertama.cpp
@@ -4,13 +4,5 @@ using namespace std;
 int main()
 {//begin
 	float nJejari, nLuas;
-	cout << "Masukan jejari= ";
-	cin >> nJejari;
-	nLuas = 3.14 * nJejari * nJejari;
-	cout << "Luasnya = " << nLuas << endl;
-
-	//numeric nJejari, nLuas
-	//display 'Masukan jejari= '
-	//compute nLuas = 3.14 * r * r
 
 }
